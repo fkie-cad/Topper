cefa3a1b38a4d5f50a1c5f10232377b4aa94d42f
Pascal <pascal.kuehnemann@fkie.fraunhofer.de>
2023-07-03 13:29:36 +0200

    Added apache-commons-cli and gson for parsing command line arguments and JSON, respectively. Also removed code to test whether Junit5 works.

--- topper/src/com/topper/main/Main.java
+++ topper/src/com/topper/main/Main.java
@@ -2,16 +2,12 @@ package com.topper.main;
 
 import java.io.IOException;
 
-import org.jf.dexlib2.DexFileFactory;
-import org.jf.dexlib2.dexbacked.DexBackedDexFile;
-
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		// Test push from eclipse
-		final DexBackedDexFile file = DexFileFactory.loadDexFile("test123", null);
-
+		
+		
 	}
 
 }
