a8034ba9b30d06c591552e622d08ba06e11078b7
Pascal <pascal.kuehnemann@fkie.fraunhofer.de>
2023-07-17 15:38:21 +0200

    Added method getPrettyInstructions to ease debugging and create readable decompilation results.

--- topper/src/com/topper/dex/decompiler/DecompilationResult.java
+++ topper/src/com/topper/dex/decompiler/DecompilationResult.java
@@ -1,11 +1,12 @@
 package com.topper.dex.decompiler;
 
+import org.jf.dexlib2.Format;
 import org.jf.dexlib2.dexbacked.DexBuffer;
 
 import com.google.common.collect.ImmutableList;
 import com.topper.dex.decompiler.instructions.DecompiledInstruction;
 
-public class DecompilationResult {
+public final class DecompilationResult {
 	
 	private final DexBuffer buffer;
 	
@@ -22,5 +23,53 @@ public class DecompilationResult {
 	
 	public final ImmutableList<DecompiledInstruction> getInstructions() {
 		return this.instructions;
+	}
+	
+	public final String getPrettyInstructions() {
+		
+		// Determine largest instruction that is neither array payload nor any switch
+		int max = 0;
+		for (final DecompiledInstruction instruction : this.getInstructions()) {
+			
+			if (max < instruction.getByteCode().length) {
+				if (instruction.getInstruction().getOpcode().format != Format.ArrayPayload &&
+						instruction.getInstruction().getOpcode().format != Format.PackedSwitchPayload && 
+						instruction.getInstruction().getOpcode().format != Format.SparseSwitchPayload) {
+					max = instruction.getByteCode().length;
+				}
+			}
+		}
+		
+		// Add instructions to output
+		final StringBuilder builder = new StringBuilder();
+		int padding;
+		int i;
+		for (final DecompiledInstruction instruction : this.getInstructions()) {
+			
+			if (instruction.getInstruction().getOpcode().format != Format.ArrayPayload &&
+						instruction.getInstruction().getOpcode().format != Format.PackedSwitchPayload && 
+						instruction.getInstruction().getOpcode().format != Format.SparseSwitchPayload) {
+				
+				// Format: <hex bytecode> <padding> <instruction string>
+				for (final byte b : instruction.getByteCode()) {
+					builder.append(String.format("%02x", b));
+					builder.append(' ');
+				}
+				
+				// Determine number of whitespace to use for padding
+				// For each byte, two chars are needed for hex, one for space.
+				padding = (max - instruction.getByteCode().length) * 3;
+				
+				for (i = 0; i < padding; i++) {
+					builder.append(' ');
+				}
+			}
+			
+			// Finally append instruction string
+			builder.append(instruction.getInstructionString());
+			builder.append(System.lineSeparator());
+		}
+		
+		return builder.toString();
 	}
 }
