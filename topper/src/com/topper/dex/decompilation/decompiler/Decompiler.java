abd63ff2799343a43ffb554e09789d447ef32008
Pascal <pascal.kuehnemann@fkie.fraunhofer.de>
2023-07-19 10:48:42 +0200

    Added javadocs to Decompiler. Previous commit added javadocs to Sweeper!

--- topper/src/com/topper/dex/decompilation/decompiler/Decompiler.java
+++ topper/src/com/topper/dex/decompilation/decompiler/Decompiler.java
@@ -2,7 +2,22 @@ package com.topper.dex.decompilation.decompiler;
 
 import com.topper.dex.decompilation.DecompilationResult;
 
+/**
+ * Decompiler interface for dex bytecode.
+ * 
+ * The default implementation is the <code>SmaliDecompiler</code>.
+ * 
+ * @author Pascal Kühnemann
+ * @see SmaliDecompiler
+ */
 public interface Decompiler {
 
+	/**
+	 * Decompiles given <code>bytes</code> into <code>DecompiledInstruction</code>s
+	 * wrapped in a <code>DecompilationResult</code>.
+	 * 
+	 * @param bytes Raw bytes to decompile into dex instructions.
+	 * @return Result wrapping decompiled instructions and further information.
+	 */
 	DecompilationResult decompile(final byte[] bytes);
 }
