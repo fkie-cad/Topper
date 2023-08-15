746a32caa4029f7531757c3b655ec24d9a8b4e24
Pascal <pascal.kuehnemann@fkie.fraunhofer.de>
2023-08-15 13:05:15 +0200

    Added javadocs to DecompilationDriver. Also added type arguments to DecompilationDriver to avoid type warnings.

--- Topper/src/main/java/com/topper/dex/decompilation/pipeline/DecompilationDriver.java
+++ Topper/src/main/java/com/topper/dex/decompilation/pipeline/DecompilationDriver.java
@@ -1,7 +1,10 @@
 package com.topper.dex.decompilation.pipeline;
 
+import java.util.Map;
+
 import org.eclipse.jdt.annotation.NonNull;
 
+import com.topper.dex.decompilation.staticanalyser.Gadget;
 import com.topper.exceptions.StageException;
 
 /**
@@ -10,45 +13,47 @@ import com.topper.exceptions.StageException;
  * decompilation {@link Pipeline}s that are responsible for
  * {@link Gadget} extraction.
  * 
- *  Initially, a default pipeline is set to ensure correct
- *  execution. However, it is possible to replace the default
- *  pipeline with any other custom pipeline. Notice that
- *  a custom pipeline must adhere to pipeline constraints.
- *  
- *  @author Pascal Kühnemann
- *  @since 07.08.2023
+ * Initially, a default pipeline is set to ensure correct
+ * execution. However, it is possible to replace the default
+ * pipeline with any other custom pipeline. Notice that
+ * a custom pipeline must adhere to pipeline constraints.
+ * 
+ * @author Pascal Kühnemann
+ * @since 07.08.2023
  * */
 public class DecompilationDriver {
 
 	/**
-	 * {@code Pipeline} to use when decompiling bytecode.
+	 * <code>Pipeline</code> to use when decompiling bytecode.
+	 * Explicitly ignore generic type.
 	 * */
 	@NonNull
-	private Pipeline pipeline;
+	private Pipeline<? extends Map<@NonNull String, @NonNull StageInfo>> pipeline;
 	
 	public DecompilationDriver() {
 		this.pipeline = Pipeline.createDefaultPipeline();
 	}
 	
 	/**
-	 * Attempts to decompile whatever is specified in {@code args}.
+	 * Attempts to decompile whatever is specified in <code>args</code>.
 	 * 
 	 * Depending on the underlying implementations, this method can
 	 * be time consuming.
 	 * 
 	 * @param args Description of the arguments required for pipeline execution.
+	 * 	Different {@link Stage}s may require different arguments.
 	 * @return Result representing the output of the pipeline.
 	 * @throws StageException If an error occurs within a {@link Stage} in the {@code Pipeline}.
 	 * */
 	@NonNull
-	public final PipelineResult decompile(@NonNull final PipelineArgs args) throws StageException {
+	public final PipelineResult<? extends Map<@NonNull String, @NonNull StageInfo>> decompile(@NonNull final PipelineArgs args) throws StageException {
 		return this.pipeline.execute(args);
 	}
 	
 	/**
 	 * Overwrites the current pipeline with a custom pipeline.
 	 * */
-	public final void setPipeline(@NonNull final Pipeline pipeline) {
+	public final void setPipeline(@NonNull final Pipeline<? extends Map<@NonNull String, @NonNull StageInfo>> pipeline) {
 		this.pipeline = pipeline;
 	}
 }
